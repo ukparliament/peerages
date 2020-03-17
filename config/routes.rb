84d5d7c6b45b9a24df7cfcbf5ba9378430ac7773
Michael Smethurst <michael@smethur.st>
2020-03-17 16:32:09 +0000

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
   
   get 'peerages' => 'peerage#index', as: :peerage_list
   get 'peerages/:peerage' => 'peerage#show', as: :peerage_show
