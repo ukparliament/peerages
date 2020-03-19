2b01b743cc7909626adc55f0d04b10165156206b
Robert Brook <robertbrook@fastmail.fm>
2020-03-19 14:45:14 +0000

    styles

--- app/models/administration.rb
+++ app/models/administration.rb
@@ -1,4 +1,11 @@
 class Administration < ActiveRecord::Base
   
   has_many :peerages, -> { order( :patent_on ) }
+  
+  def prime_minister_ordinally
+    prime_minister.gsub(/ \((.*)\)/) {|s| ', ' + s[2].to_i.ordinalize + ' administration'}
+  end
+  
 end
+
+
