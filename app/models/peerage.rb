5ad47834563bfb034664828fcc378b6752043f6a
Michael Smethurst <michael@smethur.st>
2020-07-21 13:54:35 +0100

    association with person

--- app/models/peerage.rb
+++ app/models/peerage.rb
@@ -4,6 +4,7 @@ class Peerage < ActiveRecord::Base
   belongs_to :peerage_type
   belongs_to :rank
   belongs_to :announcement
+  belongs_to :person
   has_many :law_lords, -> { order( :appointed_on ) }
   has_many :subsidiary_titles, -> { order( :title ) }
 end
