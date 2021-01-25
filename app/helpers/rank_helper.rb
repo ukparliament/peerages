a4415327ff85f12bfbcdefeedfd1e60fcd5f0752
Michael Smethurst <michael@smethur.st>
2021-01-25 21:40:39 +0000

    added methods for display of gendered rank label with link

--- app/helpers/rank_helper.rb
+++ app/helpers/rank_helper.rb
@@ -1,2 +1,20 @@
 module RankHelper
+  
+  def male_title( rank )
+    gendered_rank_label = rank.male_gendered_label
+    if gendered_rank_label
+      link_to( gendered_rank_label.label, rank_show_url( :gendered_rank_label => gendered_rank_label ) )
+    else
+      '-'
+    end
+  end
+  
+  def female_title( rank )
+    gendered_rank_label = rank.female_gendered_label
+    if gendered_rank_label
+      link_to( gendered_rank_label.label, rank_show_url( :gendered_rank_label => gendered_rank_label ) )
+    else
+      '-'
+    end
+  end
 end
