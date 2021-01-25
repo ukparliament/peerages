ece212e556dbad1ec4b16f1f86f885b4ae3440a0
Michael Smethurst <michael@smethur.st>
2021-01-25 13:40:04 +0000

    basic special remainder model

---
+++ app/models/special_remainder.rb
@@ -0,0 +1,2 @@
+class SpecialRemainder < ActiveRecord::Base
+end
---
+++ test/models/special_remainder_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SpecialRemainderTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
