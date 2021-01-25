8bbd1821e421ab3eca254938d3e6d8571eacc052
Michael Smethurst <michael@smethur.st>
2021-01-25 14:02:43 +0000

    added link to special remainders

--- app/models/peerage.rb
+++ app/models/peerage.rb
@@ -4,7 +4,7 @@ class Peerage < ActiveRecord::Base
   belongs_to :peerage_type
   belongs_to :rank
   belongs_to :announcement
-  belongs_to :person
+  belongs_to :special_remainder
   has_many :law_lords, -> { order( :appointed_on ) }
   has_many :subsidiary_titles, -> { order( :title ) }
 end
