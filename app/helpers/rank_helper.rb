e058c25500d34c2fbfcb5e2cec2e7bba647ea94e
Michael Smethurst <michael@smethur.st>
2021-02-02 22:22:05 +0000

    removed links

--- app/helpers/rank_helper.rb
+++ app/helpers/rank_helper.rb
@@ -3,7 +3,7 @@ module RankHelper
   def male_title( rank )
     gendered_rank_label = rank.male_gendered_label
     if gendered_rank_label
-      link_to( gendered_rank_label.label, rank_show_url( :gendered_rank_label => gendered_rank_label ) )
+      gendered_rank_label.label
     else
       '-'
     end
@@ -12,7 +12,7 @@ module RankHelper
   def female_title( rank )
     gendered_rank_label = rank.female_gendered_label
     if gendered_rank_label
-      link_to( gendered_rank_label.label, rank_show_url( :gendered_rank_label => gendered_rank_label ) )
+      gendered_rank_label.label
     else
       '-'
     end
