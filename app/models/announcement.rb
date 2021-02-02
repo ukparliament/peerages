3b01f9ce54d64bee4ef0ae6e49b60abc11a0945b
Michael Smethurst <michael@smethur.st>
2021-02-02 22:23:27 +0000

    added link to letters patent

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,4 +2,5 @@ class Announcement < ActiveRecord::Base
   
   belongs_to :announcement_type
   has_many :peerages, -> { order( :patent_on ) }
+  has_many :letters_patents, -> { order( :patent_on ) }
 end
