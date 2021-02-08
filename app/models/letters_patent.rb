7204d1ac36ba77a65c7d3b8836dd4aef2eeb33b8
Michael Smethurst <michael@smethur.st>
2021-02-08 14:44:18 +0000

    added legislation citation

--- app/models/letters_patent.rb
+++ app/models/letters_patent.rb
@@ -1,5 +1,8 @@
 class LettersPatent < ActiveRecord::Base
   
+  # Legislation covering letters patent
+  # https://www.legislation.gov.uk/uksi/1992/1730/made
+  
   belongs_to :administration
   belongs_to :peerage_type
   belongs_to :announcement
