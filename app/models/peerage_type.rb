8871f38f1f7d4b8c7c13eec9fe40cbae00cc6a4a
Michael Smethurst <michael@smethur.st>
2021-02-22 17:09:16 +0000

    added peerages with letter model

--- app/models/peerage_type.rb
+++ app/models/peerage_type.rb
@@ -1,4 +1,8 @@
 class PeerageType < ActiveRecord::Base
   
   has_many :peerages, -> { order( :alpha ) }
+  
+  def peerages_with_letter( letter )
+    Peerage.all.where( 'peerage_type_id = ?', self ).where( 'letter_id = ?', letter ).order( 'alpha' )
+  end
 end
