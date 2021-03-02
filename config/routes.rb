cf54e42cc025914a88656cfe8b10939bb3d49baa
Michael Smethurst <michael@smethur.st>
2021-03-02 13:22:25 +0000

    removed all except home

--- config/routes.rb
+++ config/routes.rb
@@ -4,53 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  
-  get 'people/a-z' => 'person_atoz#index', as: :person_atoz_list
-  get 'people/a-z/:letter' => 'person_atoz#show', as: :person_atoz_show
-  
-  get 'people' => 'person#index', as: :person_list
-  get 'people/:person' => 'person#show', as: :person_show
-  
-  get 'administrations' => 'administration#index', as: :administration_list
-  get 'administrations/:administration' => 'administration#show', as: :administration_show
-  
-  get 'special-remainders' => 'special_remainder#index', as: :special_remainder_list
-  get 'special-remainders/:special_remainder' => 'special_remainder#show', as: :special_remainder_show
-  
-  get 'ranks' => 'rank#index', as: :rank_list
-  get 'ranks/:rank' => 'rank#show', as: :rank_show
-  
-  get 'peerage-types' => 'peerage_type#index', as: :peerage_type_list
-  get 'peerage-types/:peerage_type' => 'peerage_type#show', as: :peerage_type_show
-  
-  get 'peerage-types/:peerage_type/a-z' => 'peerage_type_atoz#index', as: :peerage_type_atoz_list
-  get 'peerage-types/:peerage_type/a-z/:letter' => 'peerage_type_atoz#show', as: :peerage_type_atoz_show
-  
-  get 'jurisdictions' => 'jurisdiction#index', as: :jurisdiction_list
-  get 'jurisdictions/:jurisdiction' => 'jurisdiction#show', as: :jurisdiction_show
-  
-  get 'announcement-types' => 'announcement_type#index', as: :announcement_type_list
-  get 'announcement-types/:announcement_type' => 'announcement_type#show', as: :announcement_type_show
-  
-  get 'announcements' => 'announcement#index', as: :announcement_list
-  get 'announcements/:announcement' => 'announcement#show', as: :announcement_show
-  
-  get 'peerages/a-z' => 'peerage_atoz#index', as: :peerage_atoz_list
-  get 'peerages/a-z/:letter' => 'peerage_atoz#show', as: :peerage_atoz_show
-  
-  get 'peerages' => 'peerage#index', as: :peerage_list
-  get 'peerages/:peerage' => 'peerage#show', as: :peerage_show
-  
-  get 'letters-patent' => 'letters_patent#index', as: :letters_patent_list
-  get 'letters-patent/:letters_patent' => 'letters_patent#show', as: :letters_patent_show
-  
-  get 'law-lords' => 'law_lord#index', as: :law_lord_list
-  get 'law-lords/:law_lord' => 'law_lord#show', as: :law_lord_show
-  
-  get 'meta' => 'meta#index', as: :meta_list
-  get 'meta/about' => 'meta#about', as: :meta_about
-  get 'meta/iaq' => 'meta#iaq', as: :meta_iaq
-  get 'meta/schema' => 'meta#schema', as: :meta_schema
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
