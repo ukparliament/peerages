d9c947bff8a12ed673433a1c2bbf58c645801c0d
Michael Smethurst <michael@smethur.st>
2021-03-05 19:08:13 +0000

    readded

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+DB_HOST="peerages.postgres.database.azure.com"
+DB_DATABASE="peerages"
+DB_USERNAME="<username>"
+DB_PASSWORD="<password>"
+
+
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
