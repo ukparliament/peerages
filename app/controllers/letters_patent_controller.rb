b1062c916b0812a22485756d92920b893438bfb9
Michael Smethurst <michael@smethur.st>
2021-03-15 21:50:45 +0000

    query as set of left joins so nothing goes missing

--- app/controllers/letters_patent_controller.rb
+++ app/controllers/letters_patent_controller.rb
@@ -1,7 +1,7 @@
 class LettersPatentController < ApplicationController
   
   def index
-    @letters_patent = LettersPatent.all.joins( 'as lp, letters_patent_times as lpt, administrations as a, people as p' ).select( 'lp.*, lpt.label as letters_patent_time_inline, a.prime_minister as prime_minister_inline, p.forenames as person_forenames_inline, p.surname as person_surname_inline, p.date_of_death as person_date_of_death_inline' ).where( 'lp.letters_patent_time_id = lpt.id and lp.administration_id = a.id and lp.person_id = p.id' ).order( 'lp.patent_on' ).order( 'lp.ordinality_on_date' )
+    @letters_patent = LettersPatent.find_by_sql( "SELECT lp.*, lpt.label AS letters_patent_time_inline, a.prime_minister AS prime_minister_inline, p.forenames AS person_forenames_inline, p.surname AS person_surname_inline, p.date_of_death AS person_date_of_death_inline FROM letters_patents lp LEFT JOIN letters_patent_times lpt ON lp.letters_patent_time_id = lpt.id  LEFT JOIN administrations a ON lp.administration_id = a.id LEFT JOIN people p ON lp.person_id = p.id ORDER BY lp.patent_on, lp.ordinality_on_date;" )
     respond_to do |format|
       format.html
       format.tsv
