b6489f1560ad0881b678a78cfe4481edbdc2612c
Michael Smethurst <michael@smethur.st>
2021-03-15 20:44:26 +0000

    bring fields from join tables inline

--- app/controllers/letters_patent_controller.rb
+++ app/controllers/letters_patent_controller.rb
@@ -1,7 +1,7 @@
 class LettersPatentController < ApplicationController
   
   def index
-    @letters_patent = LettersPatent.all.order( 'patent_on' ).order( 'ordinality_on_date' )
+    @letters_patent = LettersPatent.all.joins( 'as lp, letters_patent_times as lpt, administrations as a, people as p' ).select( 'lp.*, lpt.label as letters_patent_time_inline, a.prime_minister as prime_minister_inline, p.forenames as person_forenames_inline, p.surname as person_surname_inline, p.date_of_death as person_date_of_death_inline' ).where( 'lp.letters_patent_time_id = lpt.id and lp.administration_id = a.id and lp.person_id = p.id' ).order( 'lp.patent_on' ).order( 'lp.ordinality_on_date' )
     respond_to do |format|
       format.html
       format.tsv
