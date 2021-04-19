74a4cd9863115183fa3fdc1f388688c4d4e1be68
Michael Smethurst <michael@smethur.st>
2021-04-19 17:38:44 +0100

    basic list and show methods

--- app/controllers/kingdom_controller.rb
+++ app/controllers/kingdom_controller.rb
@@ -1,2 +1,11 @@
 class KingdomController < ApplicationController
+  
+  def index
+    @kingdoms = Kingdom.all.order( 'end_on, start_on')
+  end
+  
+  def show
+    kingdom = params[:kingdom]
+    @kingdom = Kingdom.find( kingdom )
+  end
 end
