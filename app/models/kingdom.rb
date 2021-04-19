9e628f6e2d918feb8662951e1fb957e359ad14a1
Michael Smethurst <michael@smethur.st>
2021-04-19 17:39:14 +0100

    methods for dates and name with dates

--- app/models/kingdom.rb
+++ app/models/kingdom.rb
@@ -1,2 +1,18 @@
 class Kingdom < ActiveRecord::Base
+  
+  def name_with_dates
+    name_with_dates = self.name
+    name_with_dates += ' ('
+    name_with_dates += self.dates
+    name_with_dates += ')'
+    name_with_dates
+  end
+  
+  def dates
+    dates = ''
+    dates += self.start_on.strftime( '%-d %B %Y') if self.start_on
+    dates += ' - '
+    dates += self.end_on.strftime( '%-d %B %Y') if self.end_on
+    dates
+  end
 end
