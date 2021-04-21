736aa127bc69922e9e56f5d45bf5f1cd76dbb4d9
Michael Smethurst <michael@smethur.st>
2021-04-21 20:40:41 +0100

    linked to peerages, added peerages by letter method

--- app/models/kingdom.rb
+++ app/models/kingdom.rb
@@ -1,5 +1,7 @@
 class Kingdom < ActiveRecord::Base
   
+  has_many :peerages, -> { order( :alpha ) }
+  
   def name_with_dates
     name_with_dates = self.name
     name_with_dates += ' ('
@@ -14,5 +16,9 @@ class Kingdom < ActiveRecord::Base
     dates += ' - '
     dates += self.end_on.strftime( '%-d %B %Y') if self.end_on
     dates
+  end
+  
+  def peerages_by_letter( letter )
+    Peerage.all.where( 'letter_id = ?', letter.id ).where( 'kingdom_id = ?', self.id ).order( 'title' )
   end
 end
