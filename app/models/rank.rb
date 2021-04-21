f12da05be51fc485b82ee273b3bb383081450235
Michael Smethurst <michael@smethur.st>
2021-04-21 21:17:45 +0100

    always list all kingdoms

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -17,13 +17,13 @@ class Rank < ActiveRecord::Base
   def label_with_kingdoms
     label = self.label
     # Only list kingdoms if the rank doesn't apply to all of them.
-    if self.kingdoms.size < 5
+    #if self.kingdoms.size < 5
       label += ' - '
       self.kingdoms.each do |kingdom|
         label += kingdom.name
         label += ', ' unless kingdom == self.kingdoms.last
       end
-    end
+      #end
     label
   end
 end
