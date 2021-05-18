67b1e38ccd8c923e03c9530f8564fb1f1f29b002
Michael Smethurst <michael@smethur.st>
2021-05-18 15:50:52 +0100

    method to return reigns with kingdom

--- app/models/monarch.rb
+++ app/models/monarch.rb
@@ -1,2 +1,6 @@
 class Monarch < ActiveRecord::Base
+  
+  def reigns
+    Reign.all.select( 'r.*, k.name as kingdom_name' ).joins( 'as r, kingdoms as k' ).where( 'r.kingdom_id = k.id' ).where( 'r.monarch_id = ?', self ).order( 'r.start_on' )
+  end
 end
