5155ecfe4293d42200460c3259985697c16bb0f8
Michael Smethurst <michael@smethur.st>
2021-05-18 22:06:52 +0100

    added associations to administration and letters patent

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,7 +1,9 @@
 class Announcement < ActiveRecord::Base
   
   belongs_to :announcement_type
+  belongs_to :administration
   has_many :peerages, -> { order( :patent_on ) }
+  has_many :letters_patent, -> { order( :patent_on, :ordinality_on_date ) }
   
   def letters_patents
     @letters_patent = LettersPatent.find_by_sql( "SELECT lp.id, lp.patent_on, lp.previous_rank, lp.previous_title, lp.administration_id,  lp.person_id, lpt.label AS letters_patent_time_inline, a.prime_minister AS prime_minister_inline, p.id AS person_id_inline, p.forenames AS person_forenames_inline, p.surname AS person_surname_inline, p.date_of_death AS person_date_of_death_inline FROM letters_patents lp LEFT JOIN letters_patent_times lpt ON lp.letters_patent_time_id = lpt.id  LEFT JOIN administrations a ON lp.administration_id = a.id LEFT JOIN people p ON lp.person_id = p.id  WHERE lp.announcement_id = #{self.id} ORDER BY lp.patent_on, lp.ordinality_on_date;" )
