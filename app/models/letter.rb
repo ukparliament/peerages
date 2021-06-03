0e3cc94cca3b4bbe0d292a07b8b37208437da3f5
Michael Smethurst <michael@smethur.st>
2021-06-03 08:28:03 +0100

    order peerages by title not alpha

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,5 +1,5 @@
 class Letter < ActiveRecord::Base
   
   has_many :people, -> { order( :surname, :forenames, :date_of_birth ) }
-  has_many :peerages, -> { order( :alpha ) }
+  has_many :peerages, -> { order( :title ) }
 end
