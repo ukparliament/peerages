38d3c572544120e3fd3003e335608f4c8a755552
Michael Smethurst <michael@smethur.st>
2021-06-03 08:28:34 +0100

    order peerages by title not alpha

--- app/models/peerage_type.rb
+++ app/models/peerage_type.rb
@@ -3,6 +3,6 @@ class PeerageType < ActiveRecord::Base
   has_many :peerages, -> { order( :alpha ) }
   
   def peerages_with_letter( letter )
-    Peerage.all.where( 'peerage_type_id = ?', self ).where( 'letter_id = ?', letter ).order( 'alpha' )
+    Peerage.all.where( 'peerage_type_id = ?', self ).where( 'letter_id = ?', letter ).order( 'title' )
   end
 end
