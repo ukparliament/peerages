87280c4ca0bbd357b9a62c8252ff33c15a75bc06
Michael Smethurst <michael@smethur.st>
2021-06-03 12:48:44 +0100

    changed @letters_patents to @letters_patent so it matches other controllers

--- app/controllers/kingdom_letters_patent_controller.rb
+++ app/controllers/kingdom_letters_patent_controller.rb
@@ -3,6 +3,13 @@ class KingdomLettersPatentController < ApplicationController
   def index
     kingdom = params[:kingdom]
     @kingdom = Kingdom.find( kingdom )
-    @letters_patents = @kingdom.letters_patent
+    @letters_patent = @kingdom.letters_patent
+
+    respond_to do |format|
+      format.html
+      format.tsv {
+        render :template => 'letters_patent/index'
+      }
+    end
   end
 end
